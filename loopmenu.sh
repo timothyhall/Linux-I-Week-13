535d87907e3dc38dca1f7a7f254bbded753297c2
Timothy Hall <timothyhall@users.noreply.github.com>
2018-04-15 03:26:24 -0500

    mchecker done
    mchecker likely done
    starting on loopmenu

---
+++ loopmenu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Timothy Hall
+#April 15, 2018
+#This program will display a menu of food options
+
+
+while [[ condition ]]; do
+  #statements
+done
+echo 1) Steak and potatoes
+echo 2) Fish and chips
+echo 3) Soup and salad
+echo Please make a selection
--- mchecker.sh
+++ mchecker.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-MAILFILE=/var/spool/mail/halltim
-MAILSIZE1=$(stat -c%s "$MAILFILE")
-echo "Size of $MAILFILE = $MAILSIZE bytes."
+#MAILFILE=/var/spool/mail/halltim
+#MAILSIZE1=$(stat -c%s "$MAILFILE")
+# echo "Size of $MAILFILE = $MAILSIZE bytes."
+account=$USER
+
+echo "$account, You've got mail!"
+
+while true ; do
+      MAILSIZE=$(ls -l /var/spool/mail/$account | wc -c)
+      sleep 30
+      NEWMAILSIZE=$(ls -l /var/spool/mail/$account | wc -c)
+      if [ $MAILSIZE -lt $NEWMAILSIZE ]
+      then
+              echo "You've got mail!"
+      else
+              sleep 30
+      fi
+done
